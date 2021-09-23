bb8c5a1479017a753bcfc282693675285a743dfe
Rodinei Fagundes <rodinei.jf@gmail.com>
2021-09-23 14:49:59 -0300

    alterando uso do path separator para o da class Platform

--- lib/core/utils/directory_utils.dart
+++ lib/core/utils/directory_utils.dart
@@ -8,12 +8,12 @@ class DirectoryUtils {
     if (Directory(path).existsSync()) {
       return;
     }
-    var complete = path.split('\\')[0];
+    var complete = path.split(Platform.pathSeparator)[0];
     final exist = Directory(complete).existsSync();
     if (!exist) Directory(complete).createSync();
-    
-    for (var i = 1; i < path.split('\\').length; i++) {
-      final e = path.split('\\')[i];
+
+    for (var i = 1; i < path.split(Platform.pathSeparator).length; i++) {
+      final e = path.split(Platform.pathSeparator)[i];
       if (e.isEmpty) continue;
 
       complete += '/$e';
