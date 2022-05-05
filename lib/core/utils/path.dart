89dbb2f2caa31ec93ea00ebaae09ac6b1e2877b7
jheime.silveira <jheime.silveira@condoconta.com.br>
2022-05-05 10:16:08 -0300

    correção: aplicar separador de pastas igual para qualquer plataforma platform-path-separator

--- lib/core/utils/path.dart
+++ lib/core/utils/path.dart
@@ -3,9 +3,8 @@ import 'dart:io';
 import 'package:path/path.dart' as p;
 
 String normalize(String path) {
-  path = p.normalize(path);
   path = path.replaceAll('/', Platform.pathSeparator);
   path = path.replaceAll('\\', Platform.pathSeparator);
-  
+  path = p.normalize(path);
   return path;
 }
